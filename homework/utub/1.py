5698a58454950e35738eeafcf95dc63adc45c478
Turuall <turusin@mail.ru>
2022-12-14 23:37:24 +0800

    с ютуба добавил

---
+++ homework/utub/1.py
@@ -0,0 +1,62 @@
+
+
+
+
+variable = 45
+
+print(variable)
+print(type(variable))
+
+
+
+variable = 'строка "первая"'
+
+print(variable)
+print(type(variable))
+
+
+variable1 = f'строка выведет значение number = {number}'
+
+
+
+# разделение строк
+my_list = variable.split(' ')
+
+
+# убрать пробелы на пустоту
+my_list = variable.replace(' ', '-')
+   
+
+# поменять маленькие е на большие Е
+my_list = variable.replace('e','E')  
+
+
+
+
+for item in my_list:
+    new_item.append(item.split('e')) 
+
+
+
+# строки наверху
+# идём на джоины
+
+# склеивание
+my_string = ''.join(my_list)
+print(my_string)
+
+
+
+
+
+
+#не успеваю
+count = 0
+for char in variable:
+    if char == 'a':
+    
+    print(char)
+
+
+
+
